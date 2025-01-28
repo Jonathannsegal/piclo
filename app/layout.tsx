4835d5d321c655ea5aea59b789d74ae022dc36b9
Jonathan Segal <jonathannsegal@gmail.com>
2025-01-28 17:43:20 -0500

    metadata image

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,20 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "piclo",
-  description: "Have fun IRL! We text you videos from locals of their favorite things to do. It's free.",
+  description: "Find a better way to explore, travel and share your experiences!",
+  metadataBase: new URL('https://piclo.app'),
+  openGraph: {
+    title: 'piclo',
+    description: "Find a better way to explore, travel and share your experiences!",
+    images: [
+      {
+        url: '/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'piclo'
+      }
+    ]
+  },
 };
 
 export default function RootLayout({
